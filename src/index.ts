15ba7c999af4ce6a7acaf962836fd7242162fbbb
Juan Quino <quinojuan@gmail.com>
2023-04-03 00:36:50 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
 console.log("Hola Juan")
+
+// Declaracion de variables:
+
+var nombre = "Martin";
+let email = "quinojuan@gmail.com"
+
+
+console.log("Hola, " + nombre);
+console.log("¿Qué tal", nombre, "?");
+console.log(`¿Cómo han ido las vacaciones, ${nombre}?`)
+
+console.log(`El Email de ${nombre} es ${email}`);
+
+const PI = 3.1416;
+
+var error: boolean;
+error = false;
+
+console.log(`Hay error?: ${error}`);
+
+// Instanciación múltiple de variables:
+
+let a:string, b:boolean, c:number;
+a = "TypeScript";
+b = true;
+c = 8.9;
+
+// BuiltIn Types: number, string, boolean, void, null and undefined
+
+
+// Tipos más complejos
+let listaTareas: string[] = ["Tarea 1", "Tarea 2"];
+
+// Lista de cadenas de texto:
+let valores:(string | number | boolean)[] = [false, "hola", 56]
