0ca6a63ddd177147ae53d515b6d31aed2960d8fc
Juan Quino <quinojuan@gmail.com>
2023-04-11 21:59:25 -0300

    variostipos

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { Console, log } from "console";
 
-console.log("Hola Juan")
+console.log("Hola Juan");
 
 // Declaracion de variables:
 
 var nombre = "Martin";
-let email = "quinojuan@gmail.com"
-
+let email = "quinojuan@gmail.com";
 
 console.log("Hola, " + nombre);
 console.log("¿Qué tal", nombre, "?");
-console.log(`¿Cómo han ido las vacaciones, ${nombre}?`)
+console.log(`¿Cómo han ido las vacaciones, ${nombre}?`);
 
 console.log(`El Email de ${nombre} es ${email}`);
 
@@ -23,70 +22,135 @@ console.log(`Hay error?: ${error}`);
 
 // Instanciación múltiple de variables:
 
-let a:string, b:boolean, c:number;
+let a: string, b: boolean, c: number;
 a = "TypeScript";
 b = true;
 c = 8.9;
 
 // BuiltIn Types: number, string, boolean, void, null and undefined
 
-
 // Tipos más complejos
 let listaTareas: string[] = ["Tarea 1", "Tarea 2"];
 
 // Lista de cadenas de texto:
-let valores:(string | number | boolean)[] = [false, "hola", 56]
+let valores: (string | number | boolean)[] = [false, "hola", 56];
 
 // Enumerados
 
 enum Estados {
-    "Completado" = "C",
-    "Incompleto" = "I",
-    "Pendiente" = "P"
+  "Completado" = "C",
+  "Incompleto" = "I",
+  "Pendiente" = "P",
 }
 
 enum PuestoCarrera {
-    "Primero" = 1,
-    "Segundo",
-    "Tercero"
+  "Primero" = 1,
+  "Segundo",
+  "Tercero",
 }
 
-let estadoTarea: Estados = Estados.Incompleto
+let estadoTarea: Estados = Estados.Incompleto;
 let puestoMaraton: PuestoCarrera = PuestoCarrera.Primero;
 
-console.log(estadoTarea)
-console.log(puestoMaraton)
-
+console.log(estadoTarea);
+console.log(puestoMaraton);
 
 // Interfaces
 
 interface Tarea {
-    nombre: string,
-    estado: Estados,
-    urgencia: number
+  nombre: string;
+  estado: Estados;
+  urgencia: number;
 }
 
 // podemos crear variables que sigan la interface Tarea
 
 let tarea1: Tarea = {
-    nombre: "Tarea 1",
-    estado: Estados.Pendiente,
-    urgencia: 10
-}
+  nombre: "Tarea 1",
+  estado: Estados.Pendiente,
+  urgencia: 10,
+};
 
-console.log(`Tarea: ${tarea1.nombre}`)
+console.log(`Tarea: ${tarea1.nombre}`);
 
 // Types de TypeScript
 type Producto = {
-    precio: number,
-    nombre: string,
-    anio: number
-}
+  precio: number;
+  nombre: string;
+  anio: number;
+};
 
 let coche: Producto = {
-    nombre: "Audi",
-    precio: 45000,
-    anio: 2011
+  nombre: "Audi",
+  precio: 45000,
+  anio: 2011,
+};
+
+console.log(
+  coche.anio > 2010
+    ? `Coche: ${coche.nombre} es nuevo`
+    : `Coche: ${coche.nombre} es viejo`
+);
+
+// Funciones
+
+function saludar() {
+  let nombre = "Martin";
+
+  console.log(`Hola ${nombre}`);
 }
 
-console.log(coche.anio > 2010 ? `Coche: ${coche.nombre} es nuevo` : `Coche: ${coche.nombre} es viejo`)
+saludar();
+
+function saludarPersona(name: string) {
+  console.log(`Hola, ${name}`);
+}
+
+saludarPersona("Sara");
+
+function despedirPersona(nombre: string = "Pepe") {
+  console.log(`Adios, ${nombre}`);
+}
+
+despedirPersona();
+despedirPersona("vicente");
+
+function despedidaOpcional(nombre?: string) {
+  if (nombre) {
+    console.log(`Adios, ${nombre}`);
+  } else {
+    console.log("Adios!");
+  }
+}
+
+despedidaOpcional("carlos");
+
+function variosParams(nombre: string, apellidos?: string, edad: number = 18) {
+  if (apellidos) {
+    console.log(`${nombre} ${apellidos} tiene ${edad} años`);
+  } else {
+    console.log(`${nombre} tiene ${edad} años`);
+  }
+}
+
+variosParams("juan", undefined, 30);
+
+function ejemploVariosTipos(a: string | number) {
+  if (typeof a === "string") {
+    console.log("A es un string");
+  } else if (typeof a === "number") {
+    console.log("a es un number");
+  } else {
+    console.log("a no es un string ni tampoco un number");
+    throw Error("A no es un string ni un number");
+  }
+}
+
+
+ejemploVariosTipos("juan")
+
+function ejemploReturn(nombre: string, apellidos: string):string | number { // le puedo decir que devuelva o un string o un number
+  return 3
+}
+
+console.log(ejemploReturn("juan", "quino"))
